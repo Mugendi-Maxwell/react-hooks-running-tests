579c40e0529fcaaa957bdcf39c75cf6ce98c4e38
Maxi-arch <mxmu025@gmail.com>
2024-10-28 21:54:14 +0300

    1stcommit

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,6 +1,11 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+// import "@testing-library/jest-dom";
+// import { render, screen } from "@testing-library/react";
 import React from "react";
+qqq
+
+function Header(props) {
+  return <h1>hello from the header</h1>
+}
 
 import Header from "../components/Header";
 
@@ -9,3 +14,4 @@ test("displays the text 'hello from the Header!'", () => {
 
   expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
 });
+export default Header; 
